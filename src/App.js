4345717c4a3de6a6b1ff2e2fb37276f032ab6e73
Mike Teuten <mike.teuten@gmail.com>
2023-03-19 09:59:28 +0000

    Added / in app.js

--- src/App.js
+++ src/App.js
@@ -19,9 +19,9 @@ function App() {
           <Route path="portfolio3/Home/" element ={<Home/>} />
        {  /* <Route path="/portfolio3/Home" element={<Home />} />*/ }
 
-          <Route path="portfolio3/Header" element={<Header/>} />
+          <Route path="portfolio3/Header/" element={<Header/>} />
           <Route path="portfolio3/ProjectGallery/" element={<ProjectGallery />} />
-          <Route path="portfolio3/Project" element={<Project/>} />
+          <Route path="portfolio3/Project/" element={<Project/>} />
 
           {/* Define a route that will have descendant routes */}
           <Route path="portfolio3/contact/*" element={<Contact/>} />
